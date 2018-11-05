3e77815953f3613875d0a40fb915f4b6e7b841b3
chadi <chadisassi@gmail.com>
2018-11-05 21:30:53 +0100

    first commit

---
+++ src/main/java/com/joes/gestion/GestionApplication.java
@@ -0,0 +1,12 @@
+package com.joes.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionApplication.class, args);
+    }
+}
