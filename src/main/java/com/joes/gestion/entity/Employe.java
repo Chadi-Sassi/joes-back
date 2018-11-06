abb6a93968fe644a3abc19ce630a4f72a06ab477
chadi <chadisassi@gmail.com>
2018-11-06 03:48:41 +0100

    Dossier/Contrat commit

--- src/main/java/com/joes/gestion/entity/Employe.java
+++ src/main/java/com/joes/gestion/entity/Employe.java
@@ -1,5 +1,8 @@
 package com.joes.gestion.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
